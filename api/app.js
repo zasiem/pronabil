cd26eb58b63fa917ae36408613a2477cec7d6df7
Erza Putra Albasori <erzaputra9110@gmail.com>
2020-11-23 01:32:28 +0700

    add: vendor add, update, delete

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var vendorsRouter = require('./routes/vendors');
@@ -16,7 +17,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- api/routes/vendors.js
+++ api/routes/vendors.js
@@ -6,14 +6,14 @@ var response = require('../context/baseResponse');
 
 // GET Vendors listing
 router.get('/', function(req, res, next){
-  let query = "SELECT * FROM vendors JOIN vendor_products ON vendors.id = vendor_products.vendor_id";
+  let query = "SELECT * FROM vendors WHERE deleted_at IS NULL";
   connection.query(query, function (err, result, fields) {
     if (err) throw err;
     response(res, 200, 'Berhasil Mendapatkan Vendor', result);
   });
 });
 
-// GET Vendors listing
+// GET Vendors by id
 router.get('/:id', function(req, res, next){
   let id = req.params.id;
   let query = `SELECT * FROM vendors WHERE id = ${id}`;
@@ -21,6 +21,84 @@ router.get('/:id', function(req, res, next){
     if (err) throw err;
     response(res, 200, 'Berhasil Mendapatkan Vendor', result);
   });
+});
+
+// GET Vendors listing
+router.get('/:id/products', function(req, res, next){
+  let id = req.params.id;
+  let query = `SELECT * FROM vendor_products WHERE vendor_id = ${id}`;
+  connection.query(query, function (err, result, fields) {
+    if (err) throw err;
+    response(res, 200, 'Berhasil Mendapatkan Vendor Product', result);
+  });
+});
+
+// add vendors data
+router.post('/store', function(req, res, next){
+  let name = req.body.name;
+  let rating = req.body.rating;
+  let address = req.body.address;
+  let phone = req.body.phone;
+  let email = req.body.email;
+  if(name == undefined || rating == undefined || address == undefined || phone == undefined || email == undefined){
+    return response(res, 400, 'Ada data belum diisi');
+  }
+
+  let today = new Date().toISOString().slice(0, 18);
+  let query = `INSERT INTO vendors VALUES(NULL,"${name}","${rating}","${address}","${phone}","${email}","${today}",NULL,NULL)`;
+  
+  try{
+    connection.query(query, function (err, result, fields) {
+      if (err) throw err;
+      return response(res, 200, 'Berhasil Menambahkan Vendor', req.body);
+    });
+  }catch(err){
+    return response(res, 400, err);
+  }
+  
+});
+
+// update vendors data
+router.put('/:id', function(req, res, next){
+  let id = req.params.id;
+  let name = req.body.name;
+  let rating = req.body.rating;
+  let address = req.body.address;
+  let phone = req.body.phone;
+  let email = req.body.email;
+  if(name == undefined || rating == undefined || address == undefined || phone == undefined || email == undefined){
+    return response(res, 400, 'Ada data belum diisi');
+  }
+
+  let today = new Date().toISOString().slice(0, 18);
+  let query = `UPDATE vendors SET name="${name}", rating="${rating}", address="${address}", phone="${phone}", email="${email}", updated_at="${today}" WHERE id = ${id}`;
+  
+  try{
+    connection.query(query, function (err, result, fields) {
+      if (err) throw err;
+      return response(res, 200, 'Berhasil Mendapatkan Mengubah Vendor', req.body);
+    });
+  }catch(err){
+    return response(res, 400, err);
+  }
+  
+});
+
+// soft delete vendors data
+router.delete('/:id', function(req, res, next){
+  let id = req.params.id;
+  let today = new Date().toISOString().slice(0, 18);
+  let query = `UPDATE vendors SET deleted_at="${today}" WHERE id = ${id}`;
+  
+  try{
+    connection.query(query, function (err, result, fields) {
+      if (err) throw err;
+      return response(res, 200, 'Berhasil Menghapus Vendor');
+    });
+  }catch(err){
+    return response(res, 400, err);
+  }
+  
 });
 
 module.exports = router;
