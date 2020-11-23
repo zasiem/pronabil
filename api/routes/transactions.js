246420bea0ece17df377923f04ecced48cddfaab
Erza Putra Albasori <erzaputra9110@gmail.com>
2020-11-23 23:15:29 +0700

    fix: sql problem

--- api/routes/transactions.js
+++ api/routes/transactions.js
@@ -5,8 +5,13 @@ var connection = require('../context/connection');
 var response = require('../context/baseResponse');
 
 // GET transactions
-router.get('/', function(req, res, next){
-    let query ="SELECT * FROM transactions";
+router.get('/', function (req, res, next) {
+  let query = null;
+  if (req.query.date != undefined) {
+    query = `SELECT * FROM transactions WHERE created_at = "${req.query.date}"`;
+  } else {
+    query = "SELECT * FROM transactions";
+  }
   connection.query(query, function (err, result, fields) {
     if (err) throw err;
     return response(res, 200, 'Berhasil Mendapatkan Transactions', result);
@@ -14,30 +19,20 @@ router.get('/', function(req, res, next){
 });
 
 // GET History transactions
-router.get('/history', function(req, res, next){
-    let query ="SELECT * FROM transactions WHERE status = 'selesai' order by created_at DESC";
-    connection.query(query, function (err, result, fields) {
-      if (err) throw err;
-      return response(res, 200, 'Berhasil Mendapatkan History Transactions', result);
-    });
-  });
-
-  // GET by Code transactions
-router.get('/search/:code', function(req, res, next){
-  let query =`SELECT * FROM transactions WHERE code = ${req.params.code} `;
-  console.log("a");
+router.get('/history', function (req, res, next) {
+  let query = "SELECT * FROM transactions WHERE status = 'selesai' order by created_at DESC";
   connection.query(query, function (err, result, fields) {
     if (err) throw err;
-    return response(res, 200, 'Berhasil Mendapatkan Code Transactions', result);
+    return response(res, 200, 'Berhasil Mendapatkan History Transactions', result);
   });
 });
 
-  // GET by Date transactions
-router.get('/search/:date', function(req, res, next){
-  let query =`SELECT * FROM transactions WHERE created_at = ${req.params.date}`;
+// GET by Code transactions
+router.get('/search/:code', function (req, res, next) {
+  let query = `SELECT * FROM transactions WHERE code = "${req.params.code}" `;
   connection.query(query, function (err, result, fields) {
     if (err) throw err;
-    return response(res, 200, 'Berhasil Mendapatkan Date Transactions', result);
+    return response(res, 200, 'Berhasil Mendapatkan Code Transactions', result);
   });
 });
 
--- api/routes/vendors.js
+++ api/routes/vendors.js
@@ -5,7 +5,7 @@ var connection = require('../context/connection');
 var response = require('../context/baseResponse');
 
 // GET Vendors listing
-router.get('/', function(req, res, next){
+router.get('/', function (req, res, next) {
   let query = "SELECT * FROM vendors WHERE deleted_at IS NULL";
   connection.query(query, function (err, result, fields) {
     if (err) throw err;
@@ -14,9 +14,9 @@ router.get('/', function(req, res, next){
 });
 
 // GET Vendors by id
-router.get('/:id', function(req, res, next){
+router.get('/:id', function (req, res, next) {
   let id = req.params.id;
-  let query = `SELECT * FROM vendors WHERE id = ${id}`;
+  let query = `SELECT * FROM vendors WHERE id = "${id}"`;
   connection.query(query, function (err, result, fields) {
     if (err) throw err;
     response(res, 200, 'Berhasil Mendapatkan Vendor', result);
@@ -24,9 +24,9 @@ router.get('/:id', function(req, res, next){
 });
 
 // GET Vendors listing
-router.get('/:id/products', function(req, res, next){
+router.get('/:id/products', function (req, res, next) {
   let id = req.params.id;
-  let query = `SELECT * FROM vendor_products WHERE vendor_id = ${id}`;
+  let query = `SELECT * FROM vendor_products WHERE vendor_id = "${id}"`;
   connection.query(query, function (err, result, fields) {
     if (err) throw err;
     response(res, 200, 'Berhasil Mendapatkan Vendor Product', result);
@@ -34,71 +34,71 @@ router.get('/:id/products', function(req, res, next){
 });
 
 // add vendors data
-router.post('/store', function(req, res, next){
+router.post('/store', function (req, res, next) {
   let name = req.body.name;
   let rating = req.body.rating;
   let address = req.body.address;
   let phone = req.body.phone;
   let email = req.body.email;
-  if(name == undefined || rating == undefined || address == undefined || phone == undefined || email == undefined){
+  if (name == undefined || rating == undefined || address == undefined || phone == undefined || email == undefined) {
     return response(res, 400, 'Ada data belum diisi');
   }
 
   let today = new Date().toISOString().slice(0, 18);
   let query = `INSERT INTO vendors VALUES(NULL,"${name}","${rating}","${address}","${phone}","${email}","${today}",NULL,NULL)`;
-  
-  try{
+
+  try {
     connection.query(query, function (err, result, fields) {
       if (err) throw err;
       return response(res, 200, 'Berhasil Menambahkan Vendor', req.body);
     });
-  }catch(err){
+  } catch (err) {
     return response(res, 400, err);
   }
-  
+
 });
 
 // update vendors data
-router.put('/:id', function(req, res, next){
+router.put('/:id', function (req, res, next) {
   let id = req.params.id;
   let name = req.body.name;
   let rating = req.body.rating;
   let address = req.body.address;
   let phone = req.body.phone;
   let email = req.body.email;
-  if(name == undefined || rating == undefined || address == undefined || phone == undefined || email == undefined){
+  if (name == undefined || rating == undefined || address == undefined || phone == undefined || email == undefined) {
     return response(res, 400, 'Ada data belum diisi');
   }
 
   let today = new Date().toISOString().slice(0, 18);
-  let query = `UPDATE vendors SET name="${name}", rating="${rating}", address="${address}", phone="${phone}", email="${email}", updated_at="${today}" WHERE id = ${id}`;
-  
-  try{
+  let query = `UPDATE vendors SET name="${name}", rating="${rating}", address="${address}", phone="${phone}", email="${email}", updated_at="${today}" WHERE id = "${id}"`;
+
+  try {
     connection.query(query, function (err, result, fields) {
       if (err) throw err;
       return response(res, 200, 'Berhasil Mendapatkan Mengubah Vendor', req.body);
     });
-  }catch(err){
+  } catch (err) {
     return response(res, 400, err);
   }
-  
+
 });
 
 // soft delete vendors data
-router.delete('/:id', function(req, res, next){
+router.delete('/:id', function (req, res, next) {
   let id = req.params.id;
   let today = new Date().toISOString().slice(0, 18);
-  let query = `UPDATE vendors SET deleted_at="${today}" WHERE id = ${id}`;
-  
-  try{
+  let query = `UPDATE vendors SET deleted_at="${today}" WHERE id = "${id}"`;
+
+  try {
     connection.query(query, function (err, result, fields) {
       if (err) throw err;
       return response(res, 200, 'Berhasil Menghapus Vendor');
     });
-  }catch(err){
+  } catch (err) {
     return response(res, 400, err);
   }
-  
+
 });
 
 module.exports = router;
