53159c574be6e30aa4dfdfa3f4a65e7de8222337
Erza Putra Albasori <erzaputra9110@gmail.com>
2020-12-06 21:18:48 +0700

    fix: change connection to pool

--- api/context/connection.js
+++ api/context/connection.js
@@ -2,17 +2,25 @@ require('dotenv/config')
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+// connection.connect(function (err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+// });
 
+// connection.on('error', function(err) {
+//     console.log('db error', err);
+//     if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+//       handleDisconnect();                         
+//     } else {                                      
+//       throw err;                                  
+//     }
+//   });
 
 module.exports = connection;
