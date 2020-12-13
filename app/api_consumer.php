68b691e1cc4c59c4110f846c61cad83a64289ba9
Erza Putra Albasori <erzaputra9110@gmail.com>
2020-12-13 23:47:23 +0700

    add: post api good receipt to finance

--- app/api_consumer.php
+++ app/api_consumer.php
@@ -1,14 +1,32 @@
 <?php
 
-function consume($link, $header = []){
+function consume($link, $header = [])
+{
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $link);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $response = curl_exec($ch);
-    curl_close($ch);     
+    curl_close($ch);
 
     return json_decode($response, true);
 }
 
-?>
+function post_good_receipt($link, $request)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $link);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt(
+        $ch,
+        CURLOPT_POSTFIELDS,
+        "nama=".$request['name']."&tanggal=".$request['tanggal']."&ammount=".$request['amount']
+    );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+
+    curl_close($ch);
+
+    return $response;
+}
